204c1f431262065e5025496925795c7ef7dcf9b1
Danh Vo Hong Phuc <phuckg12002@gmail.com>
2023-09-22 13:15:43 +0700

    fix try-catch in ConfigReader

--- src/main/java/configuration/ConfigReader.java
+++ src/main/java/configuration/ConfigReader.java
@@ -5,12 +5,8 @@ import java.util.Properties;
 
 public class ConfigReader {
     private static FileInputStream fileInputStream;
-    public ConfigReader() {
-        try {
-            fileInputStream = new FileInputStream("D:\\Java\\BingNewsORM\\src\\main\\java\\configuration\\config.properties");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public ConfigReader() throws Exception {
+        fileInputStream = new FileInputStream("D:\\Java\\BingNewsORM\\src\\main\\java\\configuration\\config.properties");
     }
     public static String getConnectionString() throws Exception {
         Properties properties = new Properties();
